d1df7298e3c6ac2284db2d97193fe859e514c2ea
DipeshKiran <100759098+DipeshKiran@users.noreply.github.com>
2022-03-02 14:53:08 +0545

    Scope

---
+++ Scope/createScope.py
@@ -0,0 +1,5 @@
+def myfunction():
+    x = 300
+    print(x)
+
+myfunction()
---
+++ Scope/funcInsidefunc.py
@@ -0,0 +1,8 @@
+def myfunc():
+    x = 300
+    def innerfinction():
+        print(x)
+
+    innerfinction()
+
+myfunc()
